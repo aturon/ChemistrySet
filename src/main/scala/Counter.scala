97369d703ec3f7cfc43ed50d17b265a4c3741569
Aaron Turon <aaron.turon@gmail.com>
2011-07-26 23:25:51 -0400

    added stubs for Counter and RWLock

---
+++ src/main/scala/Counter.scala
@@ -0,0 +1,7 @@
+// A simple nonblocking counter.  Can be used to implement semaphores.
+
+package chemistry
+
+final class Counter {
+//  private val 
+}
--- src/main/scala/RWLock.scala
+++ src/main/scala/RWLock.scala
@@ -1,19 +1,21 @@
-// Simple boolean flag lock implementation; nonreentrant
+// Simple nonreentrant rw-lock implementation
 
 package chemistry
 
 class IllegalRelease extends Exception
 
-final class Lock {
+final class RWLock {
   private sealed abstract class LockStatus
-  private case object Locked extends LockStatus
+  private case object Exclusive extends LockStatus
+  private case class Shared(count: Ref[java.lang.Integer]) extends LockStatus
   private case object Unlocked extends LockStatus
 
   private val status = Ref[LockStatus](Unlocked)
 
-  val tryAcq: Reagent[Unit,Boolean] = status.upd[Boolean] {
-    case Locked   => (Locked, false)
-    case Unlocked => (Locked, true)
+/*
+  val tryAcqRead: Reagent[Unit,Boolean] = status.upd[Boolean] {
+    case Exclusive => (Locked, false)
+    case Unlocked  => (Locked, true)
   }
   
   val acq: Reagent[Unit,Unit] = status.upd[Unit] {
@@ -24,4 +26,5 @@ final class Lock {
     case Locked   => (Unlocked, ())
     case Unlocked => throw new IllegalRelease
   }
+*/
 }
