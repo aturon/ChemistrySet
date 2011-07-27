34996879e4444591ce170476958097eef3284bb3
Aaron Turon <aaron.turon@gmail.com>
2011-07-26 20:05:38 -0400

    switch MSQueue from sealed to final

--- src/main/scala/MSQueue.scala
+++ src/main/scala/MSQueue.scala
@@ -4,7 +4,7 @@ package chemistry
 
 import scala.annotation.tailrec
 
-sealed class MSQueue[A >: Null] {
+final class MSQueue[A >: Null] {
   private final case class Node(data: A, next: Ref[Node] = Ref(null))
   private val head = Ref(Node(null))
   private val tail = Ref(head.read!())
@@ -15,18 +15,18 @@ sealed class MSQueue[A >: Null] {
   //     case ov@Node(_, Ref(nv))     => tail.cas(ov,nv) !?; retry
   //   }
   // }
-  final val enq: Reagent[A, Unit] = loop { (x:A) =>
+  val enq: Reagent[A, Unit] = loop { (x:A) =>
     @tailrec def search: Reagent[Unit,Unit] = tail.read ! () match {
       case    Node(_, r@Ref(null)) => r.cas(null, Node(x))
       case ov@Node(_, Ref(nv))     => tail.cas(ov,nv) !? (); search
     }
     search
   }
-  final val tryDeq: Reagent[Unit, Option[A]] = head.upd[Option[A]] {
+  val tryDeq: Reagent[Unit, Option[A]] = head.upd[Option[A]] {
     case Node(_, Ref(n@Node(x, _))) => (n, Some(x))
     case emp => (emp, None)
   }
-  final val deq: Reagent[Unit, A] = head.upd[A] {
+  val deq: Reagent[Unit, A] = head.upd[A] {
     case Node(_, Ref(n@Node(x, _))) => (n, x)
   }
 }
