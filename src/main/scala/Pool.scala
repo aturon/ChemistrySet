01d5bfe03937e47c4df0e1b123ce88e661ca0a0a
Aaron Turon <turon@ccs.neu.edu>
2011-07-27 10:19:44 -0400

    renamed UnorderedBag to Pool, added take methods

--- src/main/scala/Pool.scala
+++ src/main/scala/Pool.scala
@@ -4,16 +4,17 @@ package chemistry
 
 import scala.annotation.tailrec
 
-trait DeletionFlag {
+trait Deletion {
+  def delete: Unit
   def isDeleted: Boolean
 }
 
-final class UnorderedBag[A <: DeletionFlag] {
+final class Pool[A <: Deletion] {
   final case class Node(data: A, next: Cursor)
   val cursor = new Cursor(null)
 
-  final class Cursor private[UnorderedBag](node: Node) {
-    private[UnorderedBag] val ref = Ref(node)
+  final class Cursor private[Pool](node: Node) {
+    private[Pool] val ref = Ref(node)
     @tailrec def get: Node = ref.read ! () match {
       case null => null
       case n@Node(data, next) =>
@@ -24,7 +25,14 @@ final class UnorderedBag[A <: DeletionFlag] {
     }
   }
 
-  val add: Reagent[A,Unit] = cursor.ref.upd {
+  val put: Reagent[A,Unit] = cursor.ref.upd {
     (xs,x) => (Node(x, new Cursor(xs)), ())
+  }
+  val tryTake = cursor.ref.upd[Option[A]] {
+    case null => (null, None)
+    case Node(data, next) => (next.ref.read ! (), Some(data))
+  }
+  val take = cursor.ref.upd[A] {
+    case Node(data, next) => (next.ref.read ! (), data)
   }
 }
