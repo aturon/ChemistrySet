19766c227c96748fc1a489a4b51a08066acddb61
Aaron Turon <turon@ccs.neu.edu>
2011-07-27 11:32:27 -0400

    Added simple CountDownLatch

---
+++ src/main/scala/CountDownLatch.scala
@@ -0,0 +1,11 @@
+// One-time use count-down latch
+
+package chemistry
+
+final class CountDownLatch(count: Int) {
+  private val state = new Counter(count)
+
+  val getCount  = state.get
+  val countDown = state.tryDec.map(_ => ())
+  val await     = state.get.withFilter(_ == 0)
+}
