266caf7bfbddb37debe8be2a65de125306866abc
Aaron Turon <turon@ccs.neu.edu>
2011-07-27 13:15:55 -0400

    Refactoring of all combinators to use singleton/apply pattern; introduced lift and compose combinators

--- src/main/scala/Core.scala
+++ src/main/scala/Core.scala
@@ -36,34 +36,38 @@ private sealed abstract class BacktrackCommand extends Exception
 private case object ShouldBlock extends BacktrackCommand
 private case object ShouldRetry extends BacktrackCommand
 
-private sealed abstract class ReagentK[-A,+B] {
+private sealed abstract class K[-A,+B] {
   private[chemistry] def tryReact(a: A, trans: Transaction): B
 }
+private object K {
+  final case class Bind[A,B,C](k1: A => Reagent[Unit, B], k2: K[B,C]) 
+	     extends K[A,C] {
+    def tryReact(a: A, trans: Transaction): C = k1(a).tryReact((), trans, k2)
+  }
 
-private final case class BindK[A,B,C](k1: A => Reagent[Unit, B], 
-				      k2: ReagentK[B,C]) 
-		    extends ReagentK[A,C] {
-  def tryReact(a: A, trans: Transaction): C = k1(a).tryReact((), trans, k2)
-}
+  final case class Compose[A,B,C](r: Reagent[A, B], k: K[B,C]) 
+	     extends K[A,C] {
+    def tryReact(a: A, trans: Transaction): C = r.tryReact(a, trans, k)
+  }
 
-private final case class FilterK[A,B](f: A => Boolean, k: ReagentK[A,B]) 
-		   extends ReagentK[A,B] {
-  def tryReact(a: A, trans: Transaction): B = 
-    if (f(a)) k.tryReact(a, trans) else throw ShouldBlock
-}
+  final case class Filter[A,B](f: A => Boolean, k: K[A,B]) extends K[A,B] {
+    def tryReact(a: A, trans: Transaction): B = 
+      if (f(a)) k.tryReact(a, trans) else throw ShouldBlock
+  }
 
-private object FinalK extends ReagentK[Any,Any] {
-  // ID continuation at the moment; eventually will be responsible for kCAS
-  def tryReact(a: Any, trans: Transaction): Any = a
+  object Final extends K[Any,Any] {
+    // ID continuation at the moment; eventually will be responsible for kCAS
+    def tryReact(a: Any, trans: Transaction): Any = a
+  }
 }
 
 sealed abstract class Reagent[-A, +B] {
   private[chemistry] 
-  def tryReact[C](a: A, trans: Transaction, k: ReagentK[B,C]): C
+  def tryReact[C](a: A, trans: Transaction, k: K[B,C]): C
 
   final def !(a: A): B = {
     // typecast to work around contravariance and save on allocation
-    val finalk = FinalK.asInstanceOf[ReagentK[B,B]] 
+    val finalk = K.Final.asInstanceOf[K[B,B]] 
     
     def slowPath: B = {
       val status = Ref[WaiterStatus](Waiting)
@@ -101,7 +105,7 @@ sealed abstract class Reagent[-A, +B] {
 
   @inline final def !?(a:A) : Option[B] = {
     // typecast to work around contravariance and save on allocation
-    val finalk = FinalK.asInstanceOf[ReagentK[B,B]] 
+    val finalk = K.Final.asInstanceOf[K[B,B]] 
 
     try {
       Some(tryReact(a, null, finalk))
@@ -120,39 +124,60 @@ sealed abstract class Reagent[-A, +B] {
   }
 
   @inline final def flatMap[C](k: B => Reagent[Unit,C]): Reagent[A,C] = 
-    Bind(this, k)
+    bind(this, k)
   @inline final def map[C](f: B => C): Reagent[A,C] = 
-    Bind(this, (x: B) => ret(f(x)))
+    bind(this, (x: B) => ret(f(x)))
   @inline final def >>[C](k: Reagent[Unit,C]): Reagent[A,C] = 
-    Bind(this, (_:B) => k)
+    bind(this, (_:B) => k)
   @inline final def withFilter(f: B => Boolean): Reagent[A,B] =
-    PostFilter(this, f)
+    postFilter(this, f)
+  @inline final def mapFilter[C](f: PartialFunction[B, C]): Reagent[A,C] =
+    compose(this, lift(f))
   @inline final def <+>[C <: A, D >: B](that: Reagent[C,D]): Reagent[C,D] = 
-    Choice(this, that)
+    choice(this, that)
 }
 
-private final case class Bind[A,B,C](c: Reagent[A,B], k1: B => Reagent[Unit,C]) 
+private object bind {
+  final case class Bind[A,B,C](c: Reagent[A,B], k1: B => Reagent[Unit,C]) 
 	     extends Reagent[A,C] {
-  def tryReact[D](a: A, trans: Transaction, k2: ReagentK[C,D]): D = 
-    c.tryReact(a, trans, BindK(k1, k2))
+    def tryReact[D](a: A, trans: Transaction, k2: K[C,D]): D = 
+      c.tryReact(a, trans, K.Bind(k1, k2))
+  }
+  @inline def apply[A,B,C](c: Reagent[A,B], 
+			   k1: B => Reagent[Unit,C]): Reagent[A,C] = 
+    Bind(c, k1)
+}
+
+private object compose {
+  private final case class Compose[A,B,C](r1: Reagent[A,B], r2: Reagent[B,C])
+		     extends Reagent[A,C] {
+    def tryReact[D](a: A, trans: Transaction, k: K[C,D]): D =
+      r1.tryReact(a, trans, K.Compose(r2, k))
+  }
+  @inline def apply[A,B,C](r1: Reagent[A,B], r2: Reagent[B,C]): Reagent[A,C] =
+    Compose(r1, r2)
 }
 
 // PreFilter is not yet exposed as a combinator
-private final case class PreFilter[A,B](c: Reagent[A,B], f: A => Boolean) 
+private case class PreFilter[A,B](c: Reagent[A,B], f: A => Boolean) 
 		   extends Reagent[A,B] {
- def tryReact[C](a: A, trans: Transaction, k: ReagentK[B,C]): C = 
+ def tryReact[C](a: A, trans: Transaction, k: K[B,C]): C = 
    if (f(a)) c.tryReact(a, trans, k) else throw ShouldBlock
 }
 
-private final case class PostFilter[A,B](c: Reagent[A,B], f: B => Boolean) 
-		   extends Reagent[A,B] {
-  def tryReact[C](a: A, trans: Transaction, k: ReagentK[B,C]): C = 
-    c.tryReact(a, trans, FilterK(f, k))
+private object postFilter {
+  private case class PostFilter[A,B](c: Reagent[A,B], f: B => Boolean) 
+  	       extends Reagent[A,B] {
+    def tryReact[C](a: A, trans: Transaction, k: K[B,C]): C = 
+      c.tryReact(a, trans, K.Filter(f, k))
+  }
+  @inline def apply[A,B](c: Reagent[A,B], f: B => Boolean): Reagent[A,B] =
+    PostFilter(c,f)
 }
 
 object ret { 
   private final case class Ret[A](pure: A) extends Reagent[Unit,A] {
-    def tryReact[B](u: Unit, trans: Transaction, k: ReagentK[A,B]): B = 
+    def tryReact[B](u: Unit, trans: Transaction, k: K[A,B]): B = 
       k.tryReact(pure, trans)
   }
   @inline final def apply[A](pure: A): Reagent[Unit,A] = Ret(pure)
@@ -160,41 +185,57 @@ object ret {
 
 // Not sure whether this should be available as a combinaor
 // object retry extends Reagent[Any,Nothing] {
-//   final def tryReact[A](a: Any, trans: Transaction, k: ReagentK[Nothing,A]): A = 
+//   final def tryReact[A](a: Any, trans: Transaction, k: K[Nothing,A]): A = 
 //     throw ShouldRetry
 // }
 
 object never extends Reagent[Any, Nothing] {
-  def tryReact[A](a: Any, trans: Transaction, k: ReagentK[Nothing, A]): A =
+  def tryReact[A](a: Any, trans: Transaction, k: K[Nothing, A]): A =
     throw ShouldBlock
 }
 
 // this really needs a better name
 // could call it "reagent"
 object loop {
-  private final case class Loop[A,B](c: A => Reagent[Unit,B]) extends Reagent[A,B] {
-    def tryReact[C](a: A, trans: Transaction, k: ReagentK[B,C]): C = 
+  private final case class Loop[A,B](c: A => Reagent[Unit,B]) 
+		     extends Reagent[A,B] {
+    def tryReact[C](a: A, trans: Transaction, k: K[B,C]): C = 
       c(a).tryReact((), trans, k)
   }
-  @inline def apply[A,B](c: A => Reagent[Unit,B]): Reagent[A,B] = Loop(c)
+  @inline def apply[A,B](c: A => Reagent[Unit,B]): Reagent[A,B] = 
+    Loop(c)
 }
 
-private final case class Choice[A,B](r1: Reagent[A,B], r2: Reagent[A,B]) 
-	     extends Reagent[A,B] {
-  def tryReact[C](a: A, trans: Transaction, k: ReagentK[B,C]): C = 
-    try r1.tryReact(a, trans, k) catch {
-      case ShouldRetry => 
-	try r2.tryReact(a, trans, k) catch {
-	  case ShouldBlock => throw ShouldRetry
-	}
-      case ShouldBlock => r2.tryReact(a, trans, k)
-    }
+object lift {
+  private final case class Lift[A,B](f: PartialFunction[A,B]) 
+		     extends Reagent[A,B] {
+    def tryReact[C](a: A, trans: Transaction, k: K[B,C]): C =
+      if (f.isDefinedAt(a)) k.tryReact(f(a), trans) else throw ShouldBlock
+  }
+  @inline def apply[A,B](f: PartialFunction[A,B]): Reagent[A,B]  = 
+    Lift(f)
+}
+
+object choice {
+  private case class Choice[A,B](r1: Reagent[A,B], r2: Reagent[A,B]) 
+	       extends Reagent[A,B] {
+    def tryReact[C](a: A, trans: Transaction, k: K[B,C]): C = 
+      try r1.tryReact(a, trans, k) catch {
+	case ShouldRetry => 
+	  try r2.tryReact(a, trans, k) catch {
+	    case ShouldBlock => throw ShouldRetry
+	  }
+	case ShouldBlock => r2.tryReact(a, trans, k)
+      }
+  }
+  @inline def apply[A,B](r1: Reagent[A,B], r2: Reagent[A,B]): Reagent[A,B] =
+    Choice(r1, r2)
 }
 
 private final class Endpoint[A,B] extends Reagent[A,B] {
   val mq = new MSQueue[Waiter[A,B]]()
   var dual: Endpoint[B,A] = null
-  def tryReact[C](a: A, trans: Transaction, k: ReagentK[B,C]): C = 
+  def tryReact[C](a: A, trans: Transaction, k: K[B,C]): C = 
     throw ShouldRetry
 }
 object SwapChan {
@@ -216,12 +257,12 @@ final class Ref[A](init: A) extends AtomicReference[A](init) {
 //  private val waiters = new MSQueue[]()
 
   case object read extends Reagent[Unit, A] {
-    def tryReact[B](u: Unit, trans: Transaction, k: ReagentK[A,B]): B = 
+    def tryReact[B](u: Unit, trans: Transaction, k: K[A,B]): B = 
       k.tryReact(get(), trans)
   }
 
   private final class CAS(expect: A, update: A) extends Reagent[Unit, Unit] {
-    def tryReact[B](u: Unit, trans: Transaction, k: ReagentK[Unit,B]): B ={
+    def tryReact[B](u: Unit, trans: Transaction, k: K[Unit,B]): B ={
       compareAndSet(expect, update)
       k.tryReact((), trans)
     }
@@ -229,7 +270,7 @@ final class Ref[A](init: A) extends AtomicReference[A](init) {
   @inline def cas(ov:A,nv:A): Reagent[Unit,Unit] = new CAS(ov,nv) 
 
   private final class Upd[B,C](f: (A,B) => (A,C)) extends Reagent[B, C] {
-    def tryReact[D](b: B, trans: Transaction, k: ReagentK[C,D]): D = {
+    def tryReact[D](b: B, trans: Transaction, k: K[C,D]): D = {
       val ov = get()
       val (nv, ret) = f(ov, b)
       compareAndSet(ov, nv)
@@ -238,7 +279,7 @@ final class Ref[A](init: A) extends AtomicReference[A](init) {
   }
   private final class UpdUnit[B](f: PartialFunction[A, (A,B)]) 
 		extends Reagent[Unit, B] {
-    def tryReact[C](u: Unit, trans: Transaction, k: ReagentK[B,C]): C = {
+    def tryReact[C](u: Unit, trans: Transaction, k: K[B,C]): C = {
       val ov = get()
       if (!f.isDefinedAt(ov)) throw ShouldBlock
       val (nv, ret) = f(ov)
