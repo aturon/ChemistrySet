605ec8ca77d367d4729a8f624620176bd0d2ef7f
Aaron Turon <aaron.turon@gmail.com>
2011-07-28 08:56:58 -0400

    marked ElimStack final

--- src/main/scala/EliminationStack.scala
+++ src/main/scala/EliminationStack.scala
@@ -2,20 +2,20 @@
 
 package chemistry
 
-sealed class EliminationStack[A] {
+final class EliminationStack[A] {
   private val head = Ref[List[A]](List())
   private val (elimPop, elimPush) = Chan[A]()
 
-  final val push: Reagent[A,Unit] = head.upd { 
+  val push: Reagent[A,Unit] = head.upd { 
     (xs,x:A) => (x::xs, ())
   } <+> elimPush
 
-  final val tryPop: Reagent[Unit,Option[A]] = head.upd[Option[A]] {
+  val tryPop: Reagent[Unit,Option[A]] = head.upd[Option[A]] {
     case (x::xs) => (xs,  Some(x))
     case emp     => (emp, None)
   } <+> elimPop.map(Some(_))
 
-  final val pop: Reagent[Unit,A] = head.upd[A] {
+  val pop: Reagent[Unit,A] = head.upd[A] {
     case (x::xs) => (xs, x)
   } <+> elimPop
 }
