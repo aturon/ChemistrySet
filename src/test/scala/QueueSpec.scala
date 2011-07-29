44f7813421ba56ec5f0b87fae9dca95cb7b637e9
Aaron Turon <aaron.turon@gmail.com>
2011-07-29 11:42:33 -0400

    reinstated concurrent tests for MSQueue and TreiberStack

--- src/test/scala/QueueSpec.scala
+++ src/test/scala/QueueSpec.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import System.out._
 import scala.concurrent.ops._
 import com.codahale.simplespec.Spec
@@ -26,38 +27,45 @@ object QueueSpec extends Spec {
       q.enq! 2;
       (q.tryDeq! (), q.tryDeq! (), q.tryDeq! ()) must beEqualTo(Some(1), Some(2), None)
     }
-    // def `should enqueue from multiple threads in locally-ordered way` {
-    //   var q = new Queue[java.lang.Integer]()
-    //   Threads.spawnAndJoin (List(
-    // 	() => for (i <-      1 to 100000) q enqueue i,
-    // 	() => for (i <- 100001 to 200000) q enqueue i))
-    //   var left = 1
-    //   var right = 100001
-    //   var ok = true
-    //   for (ii <- q.tryDequeueAll) {
-    // 	val i = ii.intValue // yuck
-    // 	if (i <= 100000) {
-    // 	  if (left != i) {
-    // 	    print("failed at ")
-    // 	    print(left)
-    // 	    print(" got ")
-    // 	    println(i)
-    // 	    ok = false
-    // 	  }
-    // 	  left = i+1
-    // 	} else {
-    // 	  if (right != i) {
-    // 	    print("failed at ")
-    // 	    print(right)
-    // 	    print(" got ")
-    // 	    println(i)
-    // 	    ok = false
-    // 	  }
-    // 	  right = i+1	  
-    // 	}
-    //   }
-    //   ok must beTrue
-    // }
+    def `should enqueue from multiple threads in locally-ordered way` {
+      var q = new MSQueue[java.lang.Integer]()
+      Threads.spawnAndJoin (List(
+    	() => for (i <-      1 to 100000) q.enq!i,
+    	() => for (i <- 100001 to 200000) q.enq!i))
+      var left = 1
+      var right = 100001
+
+      @tailrec def check: Boolean = q.tryDeq!() match {
+	case None => true
+	case Some (ii) => {
+	  val i = ii.intValue // yuck
+	  if (i <= 100000) {
+	    if (left != i) {
+	      print("failed at ")
+	      print(left)
+	      print(" got ")
+	      println(i)
+	      false
+	    } else {
+	      left += 1
+	      check
+	    }
+	  } else {
+	    if (right != i) {
+	      print("failed at ")
+	      print(right)
+	      print(" got ")
+	      println(i)
+	      false
+	    } else {
+	      right += 1
+	      check
+	    }
+	  }
+	}
+      }
+      check must beTrue
+    }
   }
 }
 
--- src/test/scala/StackSpec.scala
+++ src/test/scala/StackSpec.scala
@@ -1,3 +1,4 @@
+import scala.annotation.tailrec
 import System.out._
 import scala.concurrent.ops._
 import com.codahale.simplespec.Spec
@@ -48,37 +49,45 @@ object StackSpec extends Spec {
     //   s push 2
     //   s.tryPopAll must beEqualTo(List(2,1))
     // }
-    // def `should push from multiple threads in locally-ordered way` {
-    //   var s = new TreiberStack[java.lang.Integer]()
-    //   Threads.spawnAndJoin (List(
-    // 	() => for (i <-      1 to 100000) s push i,
-    // 	() => for (i <- 100001 to 200000) s push i))
-    //   var left = 100000
-    //   var right = 200000
-    //   var ok = true
-    //   for (ii <- s.tryPopAll) {
-    // 	val i = ii.intValue // yuck
-    // 	if (i <= 100000) {
-    // 	  if (left != i) {
-    // 	    print("failed at ")
-    // 	    print(left)
-    // 	    print(" got ")
-    // 	    println(i)
-    // 	    ok = false
-    // 	  }
-    // 	  left = i-1
-    // 	} else {
-    // 	  if (right != i) {
-    // 	    print("failed at ")
-    // 	    print(right)
-    // 	    print(" got ")
-    // 	    println(i)
-    // 	    ok = false
-    // 	  }
-    // 	  right = i-1	  
-    // 	}
-    //   }
-    //   ok must beTrue
-    // }
+
+    def `should push from multiple threads in locally-ordered way` {
+      var s = new TreiberStack[java.lang.Integer]()
+      Threads.spawnAndJoin (List(
+    	() => for (i <-      1 to 100000) s.push ! i,
+    	() => for (i <- 100001 to 200000) s.push ! i))
+      var left = 100000
+      var right = 200000
+
+      @tailrec def check: Boolean = s.tryPop!() match {
+	case None => true
+	case Some(ii) => {
+	  val i = ii.intValue // yuck
+	  if (i <= 100000) {
+	    if (left != i) {
+	      print("failed at ")
+	      print(left)
+	      print(" got ")
+	      println(i)
+	      false
+	    } else {
+	      left -= 1
+	      check
+	    }
+	  } else {
+	    if (right != i) {
+	      print("failed at ")
+	      print(right)
+	      print(" got ")
+	      println(i)
+	      false
+	    } else {
+	      right -= 1	  
+	      check
+	    }
+	  }
+	}
+      }
+      check must beTrue
+    }
   }
 }
