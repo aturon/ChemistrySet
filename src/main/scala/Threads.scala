28f358ca3af3df55e54b315d86e4dff70f5c7590
Aaron Turon <aaron.turon@gmail.com>
2011-07-29 11:41:03 -0400

    cleaned up Threads utils, moved into chemistry package

--- src/main/scala/Threads.scala
+++ src/main/scala/Threads.scala
@@ -1,9 +1,11 @@
+package chemistry
+
 import scala.concurrent._
 import scala.concurrent.ops._
 
 object Threads {
   // launch a list of threads in parallel, and wait till they all finish
-  def spawnAndJoin(bodies: List[() => Unit]): Unit = 
+  def spawnAndJoin(bodies: Seq[() => Unit]): Unit = 
     (for (body <- bodies;
 	  done = new SyncVar[Unit];
 	  _ = spawn { body(); done set () }) 
